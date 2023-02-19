f78ebec66899b586adec96cc6c23d76c72b0fa37
Josh Hancock <livingicon@gmail.com>
2023-02-19 11:06:19 -0500

    Positioned cartNum with round cart icon

--- src/components/Nav/Cart.js
+++ src/components/Nav/Cart.js
@@ -1,13 +1,14 @@
 // Cart.js
 
 import React from "react";
+import cartOutline from "../../images/cartOutline.svg";
 
 const Cart = ({ cartItems, openCart }) => {
   
   return (
-    <div>
-      <h4 id="cartLogo" onClick={openCart}>Cart Logo</h4>
-      <div id="cartItems">
+    <div id="nav-cart">
+      <img src={cartOutline} alt="cart" id="cartIcon" onClick={openCart} />
+      <div id="cartNum">
         {cartItems.length ? cartItems.length : null}
       </div>
     </div>
