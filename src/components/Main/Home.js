124adfc0b03fb6c3baa5b493af22b3f0e396934a
Josh Hancock <livingicon@gmail.com>
2023-02-20 12:37:34 -0500

    Finish home page styling

--- src/components/Main/Home.js
+++ src/components/Main/Home.js
@@ -1,14 +1,17 @@
 // Home.js
 
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Home = () => {
 
   return (
-    <div>
-      <h1>Taking you on the journey in style</h1>
-      <p>Photo by <a href="https://unsplash.com/@jcremer?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Jérémie Crémer</a> on <a href="https://unsplash.com/photos/M5xVtIv1Lo8?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a></p>
-      <button>shop now</button>
+    <div id="home">
+      <h1 id="journey">"Going on your journey in style."</h1>
+      <p id="photoCredit">Photo by <a href="https://unsplash.com/@jcremer?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Jérémie Crémer</a> on <a href="https://unsplash.com/photos/M5xVtIv1Lo8?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a></p>
+      <Link to='/shop'>
+        <button id="shopNowBtn">shop now</button>
+      </Link>
     </div>
   )
 }
