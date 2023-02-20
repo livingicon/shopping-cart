686f9a42f4379ab0c1258693ab6227cb99e3ff6a
Josh Hancock <livingicon@gmail.com>
2023-02-20 11:58:09 -0500

    Add initial home page content

--- src/components/Main/Home.js
+++ src/components/Main/Home.js
@@ -6,7 +6,9 @@ const Home = () => {
 
   return (
     <div>
-      <h1>Combining quality, convenience, and economy, since 1979</h1>
+      <h1>Taking you on the journey in style</h1>
+      <p>Photo by <a href="https://unsplash.com/@jcremer?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Jérémie Crémer</a> on <a href="https://unsplash.com/photos/M5xVtIv1Lo8?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a></p>
+      <button>shop now</button>
     </div>
   )
 }
