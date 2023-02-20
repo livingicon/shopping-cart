1daa827aaf980b6a41e2135d31ec08e62f3297f2
Josh Hancock <livingicon@gmail.com>
2023-02-20 14:22:09 -0500

    Change cart to route to shop page when opened

--- src/components/Nav/Cart.js
+++ src/components/Nav/Cart.js
@@ -1,17 +1,21 @@
 // Cart.js
 
 import React from "react";
+import { Link } from "react-router-dom";
 import cartOutline from "../../images/cartOutline.svg";
 
 const Cart = ({ cartItems, openCart }) => {
   
   return (
-    <div id="nav-cart" onClick={openCart}>
-      <img src={cartOutline} alt="cart" id="cartIcon" />
-      <div id="cartNum" onClick={openCart}>
-        {cartItems.length ? cartItems.length : null}
+    <Link to="./shop">
+      <div id="nav-cart" onClick={openCart}>
+        <img src={cartOutline} alt="cart" id="cartIcon" />
+        <div id="cartNum" onClick={openCart}>
+          {cartItems.length ? cartItems.length : null}
+        </div>
       </div>
-    </div>
+    </Link>
+
   )
 }
 
